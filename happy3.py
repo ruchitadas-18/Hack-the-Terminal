c4011aba1aa942b398fbd7a45dadc7f784d273ef
ruchitadas-18 <80640169+ruchitadas-18@users.noreply.github.com>
2021-07-04 16:47:45 +0530

    sub code 4
    talk code of one of the bot

---
+++ happy3.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+import pyttsx3
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+talk('opening telegram')
